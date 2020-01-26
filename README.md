734c50ebe39de757976d49475441c52d9973ccdc
Ammon Smith <ammon.i.smith@gmail.com>
2020-01-26 00:17:46 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+## ref-map
+
+Rust crate for a convenience trait on `Option<T>` and `Result<T, E>`.
+
+Has no dependencies, and should work on any Rust release channel.
+
+(TODO)
+
+----
+
+Copyright (C) 2019 Ammon Smith
+
+Available under the MIT License.
---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+/*
+ * lib.rs
+ *
+ * ref-map - Convenience methods for references of Option and Result.
+ * Copyright (c) 2020 Ammon Smith
+ *
+ * ref-map is available free of charge under the terms of the MIT
+ * License. You are free to redistribute and/or modify it under those
+ * terms. It is distributed in the hopes that it will be useful, but
+ * WITHOUT ANY WARRANTY. See the LICENSE file for more details.
+ *
+ */
+
+#![deny(missing_debug_implementations)]
+
+//! TODO
+
+mod option;
+mod result;
