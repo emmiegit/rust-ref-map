ab25fa72222d17e0c427ad8210fd8595bf88b8da
Ammon Smith <ammon.i.smith@gmail.com>
2020-01-26 03:00:55 -0500

    Add README and crate docs.

--- README.md
+++ README.md
@@ -4,7 +4,22 @@ Rust crate for a convenience trait on `Option<T>` and `Result<T, E>`.
 
 Has no dependencies, and should work on any Rust release channel.
 
-(TODO)
+This crate contains two traits which can be imported to add the `ref_map` methods:
+
+```rust
+use ref_map::*;
+
+let string: Option<String> = Some("hello world\n".into());
+
+// Without ref-map:
+// the .as_ref() is necessary because otherwise it tries to consume the String
+let message: Option<&str> = string.as_ref().map(|s| s.trim());
+
+// With ref-map:
+let message: Option<&str> = string.ref_map(|s| s.strim());
+```
+
+`ref_map()` is also provided for `Result<T, E>` for `Ok`, and `ref_map_err()` for `Err`.
 
 ----
 
--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,47 @@
  */
 
 #![deny(missing_debug_implementations)]
+#![warn(missing_docs)]
 
-//! TODO
+//! Convenience methods when dealing with references of [`Option`]s and [`Results`].
+//!
+//! It introduces two traits, `OptionRefMap` and `ResultRefMap`, which add methods
+//! to their respective standard library enums to avoid needing to add `.as_ref()`
+//! before any `.map()` methods on their value.
+//!
+//! This is useful when you want to borrow from an `Option` or `Result` but want
+//! to avoid the boilerplate of first getting the references to the values contained inside.
+//!
+//! ```
+//! use ref_map::*;
+//!
+//! let string: Option<String> = Some("hello world\n".into());
+//!
+//! // Without ref-map:
+//! // the .as_ref() is necessary because otherwise it tries to consume the String
+//! let message: Option<&str> = string.as_ref().map(|s| s.trim());
+//!
+//! // With ref-map:
+//! let message: Option<&str> = string.ref_map(|s| s.trim());
+//! ```
+//!
+//! Similarly, `.ref_map()` and `.ref_map_err()` are available for `Result`s to mimic
+//! their `.map()` and `.map_err()` methods:
+//!
+//! ```
+//! # use ref_map::*;
+//! # use std::path::{Path, PathBuf};
+//! let answer: Result<PathBuf, String> = Ok(PathBuf::from("/test"));
+//!
+//! // Mapping borrowed Ok(_) to another type
+//! let path: Result<&Path, &String> = answer.ref_map(|p| p.as_path());
+//!
+//! // Mapping borrower Err(_)
+//! let error: Result<&PathBuf, &str> = answer.ref_map_err(|e| e.as_str());
+//! ```
+//!
+//! [`Option`]: https://doc.rust-lang.org/std/option/enum.Option.html
+//! [`Result`]: https://doc.rust-lang.org/std/result/enum.Result.html
 
 mod option;
 mod result;
